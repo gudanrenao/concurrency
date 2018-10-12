58326fda7cab61427c55c2140342b1f70bfa2293
gudanrenao <zhangwenit@126.com>
2018-10-12 09:06:24 +0800

    init

---
+++ src/main/java/com/zhangwen/concurrency/test/TestController.java
@@ -0,0 +1,13 @@
+package com.zhangwen.concurrency.test;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author zhangwen
+ * @since 2018/10/12 09:04
+ */
+@Controller
+public class TestController {
+
+
+}
